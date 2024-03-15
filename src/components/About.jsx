e06197cee33b3b93ec24b2fdfdbadd45746a66a0
BlackHues <123185378+BlackHues@users.noreply.github.com>
2024-03-15 10:49:23 +0530

    project done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,11 +13,14 @@ const About = () => {
         <p className='text-xl mt-20 text-justify'>
             I'm Arjun Kumar H, I am from Kollam, Kerala. I completed my Bachelor's Degree in Mechanical Engineering (2016) from Younus Institute Of Technology; Kannanalloor, Kollam. 
             After College did Technical Course in QA & QC, HVAC & MEP Engineering (2016) at Blitz Academy, Kollam  and got placed as a NDT Engineer (2017-2018) at L.G Inspection Services , Chennai. 
-            Later I joined at IFB Home Appliances & Services, Paripally as a Technician (2019-2021). I Did 1 year internship as Python Developer (2022-2023) at Futuro IT Solutions, Ernakulam</p>
+            Later I joined at IFB Home Appliances & Services, Paripally as a Technician (2019-2021). I Did 1 year internship as Python Developer (2022-2023) at Futuro IT Solutions, Ernakulam. 
+            Through consistent learning i honed my skills in Html, CSS, Javascript, Bootstrap and Frameworks like Flask, Django & React. I did projects with each frameworks. Now my passion for React.js is 
+            more and learned typescript, jsx, hooks, State Management, routing, etc... 
+            </p>
         <br />
         <p className='text-xl text-justify'>
             My family consists of Mother, Elder Brother and Myself. My hobbies are Photography, Drawing, Bike ride, Travelling, etc. I can speak Tamil (Native), English (Fluent) and Malayalam (Fluent). I have passion in self-learning and ready to face any obstacles and clear it. 
-            Please feel free to contact me at your convenience via phone at +91-9995956770 or via email at arjunh2194@gmail.com.
+            My personal goal is to get placed in a well known organization, and achieve my dreams. I am hardworking and dedicated at my work. Please feel free to contact me at your convenience via phone at +91-9995956770 or via email at arjunh2194@gmail.com.
         </p>
       </div>
     </div>
